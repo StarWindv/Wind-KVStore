0f0b9263466a874c2423dee7bec2f612592e4b2a
星灿长风v <2452234@st.shou.edu.cn>
2025-07-24 09:35:39 +0800

    Update and rename main.rs to shell_main.rs

--- src/shell_main.rs
+++ src/shell_main.rs
@@ -1,11 +1,13 @@
-// src/main.rs
-mod kvstore;
-mod shell;
-
+// src/shell-main
+pub mod kvstore;
+mod utils;
+pub mod shell;
 use anyhow::Result;
 use shell::Shell;
+
 
 fn main() -> Result<()> {
     let mut shell = Shell::new();
     shell.run()
 }
+
